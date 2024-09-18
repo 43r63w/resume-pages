32beab8e1a7dc0ed4eb8d4f595e3e552bcea4459
43r63w <145778505+43r63w@users.noreply.github.com>
2024-09-18 14:42:35 +0300

    Init project

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const cvLocation = 'https://drive.google.com/file/d/1kjXvovRtatNgO6eV8mIM8GfAzoiPf_Zg/view?usp=drive_link'
+
+
+
+function redirectToCV(){
+    window.location.href = cvLocation
+}
